6d5181067bd412985249439fb3efc8eb3549a97d
maddenvvs <maddenvvs@gmail.com>
2022-12-04 17:04:48 +0000

    Add Day01 solution

---
+++ Day01/solution.nim
@@ -0,0 +1,48 @@
+import strutils
+
+proc readProblem(): seq[string] =
+    return readFile("input.txt").strip().splitLines()
+
+iterator elfCalories(calories: seq[string]): int =
+    var currentCalories: int = 0
+
+    for line in calories:
+        if line == "":
+            yield currentCalories
+            currentCalories = 0
+        else:
+            currentCalories += parseInt(line)
+
+    if currentCalories > 0:
+        yield currentCalories
+
+proc part1(calories: seq[string]): int =
+    var maxCalories = 0
+    for elf in elfCalories(calories):
+        maxCalories = max(maxCalories, elf)
+    return maxCalories
+
+proc part2(calories: seq[string]): int =
+    var first = 0
+    var second = 0
+    var third = 0
+
+    for elf in elfCalories(calories):
+        if elf >= first:
+            third = second
+            second = first
+            first = elf
+        elif elf >= second:
+            third = second
+            second = elf
+        else:
+            third = max(third, elf)
+
+    return first + second + third
+
+proc main() =
+    let calories = readProblem()
+    echo "Part 1: ", part1(calories)
+    echo "Part 2: ", part2(calories)
+
+main()
